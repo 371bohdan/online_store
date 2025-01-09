f82fdb757b6515dc7df00265806883d7ea95014e
PetsaYuri <petsa.yuri@gmail.com>
2025-01-09 12:21:00 +0200

    Added 'mongoose' library and successfully connected to atlas mongo db server and added genericCrudController.ts file for future development. Also fixed bug with .gitingore file (renamed correctly and added .env file to list of ingored files)

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from 'express'
+import mongoose from 'mongoose';
 
 import homeRoutes from './routes/home' // потім можна змінити на інший котрий потрібен маршрут
 
@@ -13,45 +14,13 @@ app.use(express.json());
 
 app.use('/', homeRoutes);
 
+mongoose.connect(process.env.MONGODB_URI || '');
 
 const run = () => {
     app.listen(port, () => {
         console.log(`This server runs on http://localhost:${port}`);
-      });
+    });
 }
 
 
 run();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-
-// const app = express();
-
-// app.get('/', (req, res)=> {
-//     res.status(200).json({message: 'success'});
-// });
-
-
-
-// const PORT = 3000;
-
-
-
-// app.listen(PORT, () => {
-//     console.log(`This server run on http://localhost:${PORT}`);
-// });
-
-
-
---
+++ src/controllers/genericCrudController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { Document, Model } from "mongoose";
+
+const genericCrudController = <T extends Document>(Model: Model<T>) => ({
+    getAll: async (req: Request, res: Response): Promise<void> => {
+        const items = await Model.find();
+        res.json(items);
+    },
+
+    getById: async (req: Request, res: Response): Promise<void> => {
+        const { id } = req.params;
+        const item = await Model.findById(id);
+        res.json(item);
+    },
+
+    create: async (req: Request, res: Response): Promise<void> => {
+        const item = new Model(req.body);
+        res.json(await Model.create(item));
+    },
+
+    update: async (req: Request, res: Response): Promise<void> => {
+        const { id } = req.params;
+        const updatedItem = await Model.findByIdAndUpdate(id, req.body, { returnDocument: 'after' });
+        res.json(updatedItem);
+    },
+
+    removeById: async (req: Request, res: Response): Promise<void> => {
+        const { id } = req.params;
+        await Model.findByIdAndDelete(id);
+        res.send(`The '${id}' object was successfully removed.`)
+    },
+
+    removeAll: async (req: Request, res: Response): Promise<void> => {
+        await Model.deleteMany({});
+        res.send('success');
+    }
+})
+
+export default genericCrudController;
