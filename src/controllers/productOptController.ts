bb5e5d3bee410bb215cd45d81eaa83dbd1a0855d
371bohdan <haritonowpog.@gmail.com>
2025-01-19 22:52:01 +0200

    Update filter+sort

--- src/controllers/productOptController.ts
+++ src/controllers/productOptController.ts
@@ -1,26 +1,29 @@
 import { Request, Response } from "express";
-
-import Product from "../models/products"
+import Product from "../models/products";
 
 const productOptController = {
     searchForName: async (req: Request, res: Response): Promise<void> => {
         try {
-            const { title } = req.query;
+            const { title, sort } = req.query;
 
             if (!title || typeof title !== "string") {
                 res.status(400).json({ message: "Title query is required and must be a string" });
                 return;
             }
 
-            // Видаляємо лапки на початку та в кінці рядка (якщо вони є)
-
-
-            // Регулярний вираз для пошуку, щоб title починався з введеного тексту (нечутливо до регістру)
-            const products = await Product.find({
+            // Пошук за назвою та сортуванням, якщо параметр сортування є
+            let query = Product.find({
                 title: new RegExp(`^${title}`, "i"), // ^ - початок рядка
             });
 
-            res.status(200).json(products);
+            if (sort) {
+                query = query.sort({
+                    price: sort === "asc" ? 1 : -1,
+                });
+            }
+
+
+            res.status(200).json(query);
         } catch (error) {
             console.error("Error searching for products by title:", error);
             res.status(500).json({ message: "Internal server error", error });
@@ -48,6 +51,6 @@ const productOptController = {
             res.status(500).json({ message: "Internal server error", error });
         }
     }
-}
+};
 
 export default productOptController;
