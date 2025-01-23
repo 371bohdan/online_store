bf388aed92f3c5a1dbb5e03d8ad86c984130888c
371bohdan <haritonowpog.@gmail.com>
2025-01-23 19:43:34 +0200

    Lum 44

--- src/models/carts.ts
+++ src/models/carts.ts
@@ -3,51 +3,43 @@ import mongooseToSwagger from "mongoose-to-swagger";
 
 // Інтерфейс для моделі Cart
 export interface ICart extends Document {
-    userId: Types.ObjectId; // Посилання на User
-    quantity: number; // Загальна кількість товарів у кошику
-    totalPrice: number; // Загальна сума кошика
-    createdAt: Date; // Дата створення кошика
-    items: Types.ObjectId[]; // Посилання на елементи у CartItems
+    userId: Types.ObjectId; 
+    totalPrice: number; 
+    products: { productId: Types.ObjectId; count: number }[];
 }
 
 // Схема для моделі Cart
 const CartSchema = new Schema<ICart>(
     {
         userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-        quantity: {
-            type: Number,
-            required: true,
-            default: 0,
-            validate: {
-                validator: (value: number) => value >= 0,
-                message: "Quantity must be greater than or equal to 0",
-            },
-        },
         totalPrice: {
             type: Number,
-            required: true,
             default: 0,
             validate: {
                 validator: (value: number) => value >= 0,
                 message: "Total price must be greater than or equal to 0",
             },
         },
-        createdAt: {
-            type: Date,
-            default: Date.now,
+        products: {
+            type: [
+                {
+                    productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+                    count: {
+                        type: Number,
+                        required: true,
+                        default: 1,
+                        validate: {
+                            validator: (value: number) => value > 0,
+                            message: "Count must be greater than 0",
+                        },
+                    },
+                },
+            ],
+            default: [],
+             
         },
-        items: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "CartItem",
-                required: true,
-            },
-        ],
-    },
-    {
-        timestamps: true, // Додає createdAt та updatedAt автоматично
-    }
-);
+    });
+
 
 // Створення моделі Cart
 const Cart = mongoose.model<ICart>("Cart", CartSchema);
