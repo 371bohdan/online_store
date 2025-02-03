297ae0744ae3ba57d3bfa2d1686f9503f2a5b663
PetsaYuri <petsa.yuri@gmail.com>
2025-02-03 13:04:08 +0200

    Fixed bug where Swagger docs (comment) didn't change after overriding method.

--- src/routes/genericCrudRoute.ts
+++ src/routes/genericCrudRoute.ts
@@ -10,9 +10,8 @@ import { orderSwaggerSchema } from '../models/orders';
 import { verifyAdminRole } from '../controllers/authController';
 import errorHandler from '../middleware/errors/errorHandler';
 import asyncHandler from '../middleware/errors/asyncHandler';
-import mongooseToSwagger from 'mongoose-to-swagger'
 
-const genericCrudRoute = <T extends Document>(Model: Model<T>, modelName: string, methodsToSecure: Array<String>): express.Router => {
+const genericCrudRoute = <T extends Document>(Model: Model<T>, modelName: string, methodsToSecure: Array<string>): express.Router => {
     const router: express.Router = express.Router();
     const controller = genericCrudController(Model);
 
@@ -34,7 +33,6 @@ const genericCrudRoute = <T extends Document>(Model: Model<T>, modelName: string
         router.post('/', asyncHandler(controller.create));
     }
 
-
     if (methodsToSecure.includes('post')) {
         router.put('/:id', verifyAdminRole, asyncHandler(controller.update));
     } else {
@@ -54,214 +52,55 @@ const genericCrudRoute = <T extends Document>(Model: Model<T>, modelName: string
     return router;
 }
 
-function swagger(modelName: String, methodsToSecure: Array<String>): void {
-    const modelNameSingular: String = modelName.substring(0, modelName.length - 1);
+function swagger(modelName: string, methodsToSecure: Array<string>): void {
+    const modelNameSingular: string = modelName.substring(0, modelName.length - 1);
     const swaggerSchema = getTheSwaggerSchema(modelName);
     delete swaggerSchema.properties._id   //removing _id property for model Object
 
     // /api/${modelName}
-    swaggerOptions.paths[`/api/${modelName}`] = {
-        get: {
-            tags: [`${modelName} API`],
-            summary: `Get the list of ${modelName}`,
-            responses: {
-                200: {
-                    description: "Success"
-                },
-                500: {
-                    description: 'Internal server error'
-                }
-            }
-        },
+    if (swaggerOptions.paths[`/api/${modelName}`]) {
+        const overriddenMethods = Object.keys(swaggerOptions.paths[`/api/${modelName}`]);
 
-        post: {
-            tags: [`${modelName} API`],
-            summary: `create the ${modelNameSingular}`,
-            requestBody: {
-                required: true,
-                content: {
-                    "application/json": {
-                        schema: swaggerSchema
-                    }
-                }
-            },
-            responses: {
-                201: {
-                    description: "Success"
-                },
-                400: {
-                    description: `The body doesn't match the ${modelNameSingular} schema`
-                },
-                500: {
-                    description: 'Internal server error'
-                }
-            }
-        },
-
-        delete: {
-            tags: [`${modelName} API`],
-            summary: `Delete all ${modelName}`,
-            responses: {
-                200: {
-                    description: "Success"
-                },
-                500: {
-                    description: 'Internal server error'
-                }
-            }
+        if (!overriddenMethods.includes('get')) {
+            addSwaggerToGetMethod(modelName, methodsToSecure.includes('get'));
         }
+
+        if (!overriddenMethods.includes('post')) {
+            addSwaggerToPostMethod(modelName, modelNameSingular, swaggerSchema, methodsToSecure.includes('post'));
+        }
+
+        if (!overriddenMethods.includes('delete')) {
+            addSwaggerToDeleteMethod(modelName, methodsToSecure.includes('delete'));
+        }
+
+    } else {
+        swaggerOptions.paths[`/api/${modelName}`] = {};
+        addSwaggerToGetMethod(modelName, methodsToSecure.includes('get'));
+        addSwaggerToPostMethod(modelName, modelNameSingular, swaggerSchema, methodsToSecure.includes('post'));
+        addSwaggerToDeleteMethod(modelName, methodsToSecure.includes('delete'));
     }
 
     // /api/${modelName}/:id
-    swaggerOptions.paths[`/api/${modelName}/{id}`] = {
-        get: {
-            tags: [`${modelName} API`],
-            summary: `Get the ${modelNameSingular} by id`,
-            parameters: [{
-                in: 'path',
-                name: 'id',
-                required: true,
-                schema: {
-                    type: "string"
-                },
-                description: `You need to paste a ${modelNameSingular} id in the line below to get more information about the selected ${modelNameSingular}.`
-            }],
-            responses: {
-                200: {
-                    description: "Success"
-                },
-                404: {
-                    description: `The ${modelNameSingular} not found`
-                },
-                500: {
-                    description: 'Internal server error'
-                }
-            }
-        },
+    if (swaggerOptions.paths[`/api/${modelName}/{id}`]) {
+        const overriddenMethods = Object.keys(swaggerOptions.paths[`/api/${modelName}/{id}`]);
 
-        put: {
-            tags: [`${modelName} API`],
-            summary: `Update the ${modelNameSingular} by id`,
-            parameters: [{
-                in: 'path',
-                name: 'id',
-                required: true,
-                schema: {
-                    type: "string"
-                },
-                description: `You need to paste a ${modelNameSingular} id in the line below.`
-            }],
-            requestBody: {
-                required: true,
-                content: {
-                    "application/json": {
-                        schema: swaggerSchema
-                    }
-                }
-            },
-            responses: {
-                200: {
-                    description: "Success"
-                },
-                400: {
-                    description: `The body doesn't match the ${modelNameSingular} schema`
-                },
-                404: {
-                    description: `The ${modelNameSingular} not found`
-                },
-                500: {
-                    description: 'Internal server error'
-                }
-            }
-        },
-
-        delete: {
-            tags: [`${modelName} API`],
-            summary: `Delete the ${modelNameSingular} by id`,
-            parameters: [{
-                in: 'path',
-                name: 'id',
-                required: true,
-                schema: {
-                    type: "string"
-                },
-                description: `You need to paste a ${modelNameSingular} id in the line below to successfully delete the selected ${modelNameSingular}.`
-            }],
-            responses: {
-                200: {
-                    description: "Success"
-                },
-                404: {
-                    description: `The ${modelNameSingular} not found`
-                },
-                500: {
-                    description: 'Internal server error'
-                }
-            }
-        }
-    }
-
-    //secure
-    if (methodsToSecure.includes('get')) {
-        swaggerOptions.paths[`/api/${modelName}`]['get'] = {
-            ...swaggerOptions.paths[`/api/${modelName}`]['get'],
-            security: [
-                {
-                    bearerAuth: []
-                },
-            ],
+        if (!overriddenMethods.includes('get')) {
+            addSwaggerToGetByIdMethod(modelName, modelNameSingular, methodsToSecure.includes('get'));
         }
 
-        swaggerOptions.paths[`/api/${modelName}/{id}`]['get'] = {
-            ...swaggerOptions.paths[`/api/${modelName}/{id}`]['get'],
-            security: [
-                {
-                    bearerAuth: []
-                },
-            ],
-        }
-    }
-
-    if (methodsToSecure.includes('post')) {
-        swaggerOptions.paths[`/api/${modelName}`]['post'] = {
-            ...swaggerOptions.paths[`/api/${modelName}`]['post'],
-            security: [
-                {
-                    bearerAuth: []
-                },
-            ],
-        }
-    }
-
-    if (methodsToSecure.includes('put')) {
-        swaggerOptions.paths[`/api/${modelName}/{id}`]['put'] = {
-            ...swaggerOptions.paths[`/api/${modelName}/{id}`]['put'],
-            security: [
-                {
-                    bearerAuth: []
-                },
-            ],
-        }
-    }
-
-    if (methodsToSecure.includes('delete')) {
-        swaggerOptions.paths[`/api/${modelName}`]['delete'] = {
-            ...swaggerOptions.paths[`/api/${modelName}`]['delete'],
-            security: [
-                {
-                    bearerAuth: []
-                },
-            ],
+        if (!overriddenMethods.includes('put')) {
+            addSwaggerToPutMethod(modelName, modelNameSingular, swaggerSchema, methodsToSecure.includes('put'));
         }
 
-        swaggerOptions.paths[`/api/${modelName}/{id}`]['delete'] = {
-            ...swaggerOptions.paths[`/api/${modelName}/{id}`]['delete'],
-            security: [
-                {
-                    bearerAuth: []
-                },
-            ],
+        if (!overriddenMethods.includes('delete')) {
+            addSwaggerToDeleteByIdMethod(modelName, modelNameSingular, methodsToSecure.includes('delete'));
         }
+
+    } else {
+        swaggerOptions.paths[`/api/${modelName}/{id}`] = {};
+        addSwaggerToGetByIdMethod(modelName, modelNameSingular, methodsToSecure.includes('get'));
+        addSwaggerToPutMethod(modelName, modelNameSingular, swaggerSchema, methodsToSecure.includes('put'));
+        addSwaggerToDeleteByIdMethod(modelName, modelNameSingular, methodsToSecure.includes('delete'));
     }
 }
 
@@ -282,6 +121,221 @@ export function getTheSwaggerSchema(modelName: String) {
             return orderSwaggerSchema;
         default:
             throw new Error(`Swagger schema not defined for model: ${modelName}`);
+    }
+}
+
+function addSwaggerToGetMethod(modelName: string, isSecured: boolean) {
+    swaggerOptions.paths[`/api/${modelName}`]['get'] = {
+        tags: [`${modelName} API`],
+        summary: `Get the list of ${modelName}`,
+        responses: {
+            200: {
+                description: "Success"
+            },
+            500: {
+                description: 'Internal server error'
+            }
+        }
+    }
+
+    if (isSecured) {
+        swaggerOptions.paths[`/api/${modelName}`]['get'] = {
+            ...swaggerOptions.paths[`/api/${modelName}`]['get'],
+            security: [
+                {
+                    bearerAuth: []
+                },
+            ],
+        }
+    }
+}
+
+function addSwaggerToPostMethod(modelName: string, modelNameSingular: string, swaggerSchema: object, isSecured: boolean) {
+    swaggerOptions.paths[`/api/${modelName}`]['post'] = {
+        tags: [`${modelName} API`],
+        summary: `Create the ${modelNameSingular}`,
+        requestBody: {
+            required: true,
+            content: {
+                "application/json": {
+                    schema: swaggerSchema
+                }
+            }
+        },
+        responses: {
+            201: {
+                description: "Success"
+            },
+            400: {
+                description: `The body doesn't match the ${modelNameSingular} schema`
+            },
+            500: {
+                description: 'Internal server error'
+            }
+        }
+    }
+
+    if (isSecured) {
+        swaggerOptions.paths[`/api/${modelName}`]['post'] = {
+            ...swaggerOptions.paths[`/api/${modelName}`]['post'],
+            security: [
+                {
+                    bearerAuth: []
+                },
+            ],
+        }
+    }
+}
+
+function addSwaggerToDeleteMethod(modelName: string, isSecured: boolean) {
+    swaggerOptions.paths[`/api/${modelName}`]['delete'] = {
+        tags: [`${modelName} API`],
+        summary: `Delete all ${modelName}`,
+        responses: {
+            200: {
+                description: "Success"
+            },
+            500: {
+                description: 'Internal server error'
+            }
+        }
+    }
+
+    if (isSecured) {
+        swaggerOptions.paths[`/api/${modelName}`]['delete'] = {
+            ...swaggerOptions.paths[`/api/${modelName}`]['delete'],
+            security: [
+                {
+                    bearerAuth: []
+                },
+            ],
+        }
+    }
+}
+
+function addSwaggerToGetByIdMethod(modelName: string, modelNameSingular: string, isSecured: boolean) {
+    swaggerOptions.paths[`/api/${modelName}/{id}`]['get'] = {
+        tags: [`${modelName} API`],
+        summary: `Get the ${modelNameSingular} by id`,
+        parameters: [{
+            in: 'path',
+            name: 'id',
+            required: true,
+            schema: {
+                type: "string"
+            },
+            description: `You need to paste a ${modelNameSingular} id in the line below to get more information about the selected ${modelNameSingular}.`
+        }],
+        responses: {
+            200: {
+                description: "Success"
+            },
+            404: {
+                description: `The ${modelNameSingular} not found`
+            },
+            500: {
+                description: 'Internal server error'
+            }
+        }
+
+    }
+
+    if (isSecured) {
+        swaggerOptions.paths[`/api/${modelName}/{id}`]['get'] = {
+            ...swaggerOptions.paths[`/api/${modelName}/{id}`]['get'],
+            security: [
+                {
+                    bearerAuth: []
+                },
+            ],
+        }
+    }
+}
+
+function addSwaggerToPutMethod(modelName: string, modelNameSingular: string, swaggerSchema: object, isSecured: boolean) {
+    swaggerOptions.paths[`/api/${modelName}/{id}`]['put'] = {
+        tags: [`${modelName} API`],
+        summary: `Update the ${modelNameSingular} by id`,
+        parameters: [{
+            in: 'path',
+            name: 'id',
+            required: true,
+            schema: {
+                type: "string"
+            },
+            description: `You need to paste a ${modelNameSingular} id in the line below.`
+        }],
+        requestBody: {
+            required: true,
+            content: {
+                "application/json": {
+                    schema: swaggerSchema
+                }
+            }
+        },
+        responses: {
+            200: {
+                description: "Success"
+            },
+            400: {
+                description: `The body doesn't match the ${modelNameSingular} schema`
+            },
+            404: {
+                description: `The ${modelNameSingular} not found`
+            },
+            500: {
+                description: 'Internal server error'
+            }
+        }
+    }
+
+    if (isSecured) {
+        swaggerOptions.paths[`/api/${modelName}/{id}`]['put'] = {
+            ...swaggerOptions.paths[`/api/${modelName}/{id}`]['put'],
+            security: [
+                {
+                    bearerAuth: []
+                },
+            ],
+        }
+    }
+}
+
+function addSwaggerToDeleteByIdMethod(modelName: string, modelNameSingular: string, isSecured: boolean) {
+    swaggerOptions.paths[`/api/${modelName}/{id}`]['delete'] = {
+        tags: [`${modelName} API`],
+        summary: `Delete the ${modelNameSingular} by id`,
+        parameters: [{
+            in: 'path',
+            name: 'id',
+            required: true,
+            schema: {
+                type: "string"
+            },
+            description: `You need to paste a ${modelNameSingular} id in the line below to successfully delete the selected ${modelNameSingular}.`
+        }],
+        responses: {
+            200: {
+                description: "Success"
+            },
+            404: {
+                description: `The ${modelNameSingular} not found`
+            },
+            500: {
+                description: 'Internal server error'
+            }
+        }
+    }
+
+    if (isSecured) {
+        swaggerOptions.paths[`/api/${modelName}/{id}`]['delete'] = {
+            ...swaggerOptions.paths[`/api/${modelName}/{id}`]['delete'],
+            security: [
+                {
+                    bearerAuth: []
+                },
+            ],
+        }
     }
 }
 
