76a597776003fe151447e10c6a2417529fc7f5e8
PetsaYuri <98687604+PetsaYuri@users.noreply.github.com>
2025-04-18 22:15:49 +0300

    Update jwtService.ts

--- src/api/services/auxiliary/jwtService.ts
+++ src/api/services/auxiliary/jwtService.ts
@@ -79,8 +79,8 @@ export const jwtService = {
     setRefreshTokenInCookie: (res: Response, refreshToken: string) => {
         res.cookie('jwt', refreshToken, {
             httpOnly: true,
-            sameSite: 'strict',
-            secure: true,
+            sameSite: 'Lax', //strict
+            secure: false, //true
             maxAge: ms(`${ENV.JWT_REFRESH_TOKEN_EXPIRES}d`)
         });
     },
